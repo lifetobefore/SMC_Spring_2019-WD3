05a29e5808251749853da83e1accc5d80f3042b0
lifetobefore <lifetobefore@gmail.com>
2019-06-04 05:05:16 -0700

    nav-bar responsive
    Couldnt work, index,html as template

---
+++ Final/js/main.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function(){
+    $(window).on('resize', function() {
+        var win = $(this);
+        if (win.width() > 769) {
+            $('#mySidenav').addClass('la-nav');
+            $('#mySidenav').removeClass('sidenav');
+        }else{
+            $('#mySidenav').addClass('sidenav');
+            $('#mySidenav').removeClass('la-nav');
+        }
+    })
+})*/
