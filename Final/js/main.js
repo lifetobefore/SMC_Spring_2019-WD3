7c2114b8f42be22434a538894af36efd987a8840
lifetobefore <lifetobefore@gmail.com>
2019-06-04 11:16:06 -0700

    final website
    final website

--- Final/js/main.js
+++ Final/js/main.js
@@ -10,3 +10,12 @@
         }
     })
 })*/
+
+var count = 0;
+var button = document.getElementById("countButton");
+var display = document.getElementById("displayCount");
+
+button.onclick = function(){
+  count++;
+  display.innerHTML = count;
+}
